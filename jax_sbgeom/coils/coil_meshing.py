9b2a9801ec785970f1953e11b5a6a2cbf8fc0ce0
Timo Bogaarts <t.j.bogaarts@tue.nl>
2025-10-22 17:39:32 +0200

    coil surface mesh

--- jax_sbgeom/coils/coil_meshing.py
+++ jax_sbgeom/coils/coil_meshing.py
@@ -6,7 +6,7 @@ import numpy as onp
 from typing import Literal
 from jax_sbgeom.jax_utils.utils import interpolate_array_modulo_broadcasted
 from jax_sbgeom.flux_surfaces.flux_surface_meshing import _build_triangles_surface, _build_closed_strips
-from .base_coil import Coil
+from .base_coil import FiniteSizeCoil
 
 
 def _mesh_finite_sized_lines_connectivity(n_samples : int, n_lines_per_coil : int, normal_orientation : bool):
@@ -32,7 +32,7 @@ def _mesh_finite_sized_lines_connectivity(n_samples : int, n_lines_per_coil : in
 def _mesh_rectangular_finite_sized_coils_connectivity(n_samples : int, normal_orientation : bool):
     return _mesh_finite_sized_lines_connectivity(n_samples, 4, normal_orientation)
 
-def mesh_coil_surface(coil : Coil, n_s : int, width_radial : float, width_phi : float, method : str):
+def mesh_coil_surface(coil : FiniteSizeCoil, n_s : int, width_radial : float, width_phi : float):
     '''
     Mesh the surface of a coil
 
@@ -53,6 +53,6 @@ def mesh_coil_surface(coil : Coil, n_s : int, width_radial : float, width_phi : 
     jnp.ndarray
         Connectivity array of the meshed coil surface
     '''
-    finite_size_lines = coil.finite_size(jnp.linspace(0, coil.length(), n_s), width_radial, width_phi, method, number_of_rmf_samples=n_s)
+    finite_size_lines = coil.finite_size(jnp.linspace(0, 1.0, n_s, endpoint=False), width_radial, width_phi)
     connectivity = _mesh_rectangular_finite_sized_coils_connectivity(n_s, normal_orientation=True)
     return finite_size_lines.reshape(-1, 3), connectivity
