6ff835fb3a7f2f1a57624e89681950a050e580d6
jjwalkowiak <jedrzej.walkowiakk@gmail.com>
2026-03-24 09:54:59 +0100

    update __init__.py with new functions

--- src/jax_sbgeom/coils/__init__.py
+++ src/jax_sbgeom/coils/__init__.py
@@ -2,13 +2,13 @@ from .base_coil import Coil, FiniteSizeCoil, CentroidFrame, FrenetSerretFrame, R
 from .coilset import CoilSet, FiniteSizeCoilSet
 from .fourier_coil import FourierCoil, convert_to_fourier_coilset, convert_to_fourier_coil, convert_fourier_coilset_to_equal_arclength, convert_fourier_coil_to_equal_arclength
 from .discrete_coil import DiscreteCoil
-from .coil_meshing import mesh_coil_surface, mesh_coilset_surface
+from .coil_meshing import mesh_coil_surface, mesh_coilset_surface, mesh_coil_volumetric
 from .coil_winding_surface import create_coil_winding_surface, create_optimized_coil_winding_surface, calculate_normals_from_closest_point_on_mesh
 from . import biot_savart
 __all__ = ["Coil", "FiniteSizeCoil", "CoilSet",  "CentroidFrame", "FrenetSerretFrame", "RadialVectorFrame", "RotationMinimizedFrame",
            "CoilSet", "FiniteSizeCoilSet",
            "FourierCoil", "convert_to_fourier_coilset", "convert_to_fourier_coil", "convert_fourier_coil_to_equal_arclength", "convert_fourier_coilset_to_equal_arclength",
            "DiscreteCoil", 
-            "mesh_coil_surface", "mesh_coilset_surface",
+            "mesh_coil_surface", "mesh_coilset_surface", "mesh_coil_volumetric",
             "create_coil_winding_surface", "create_optimized_coil_winding_surface", "calculate_normals_from_closest_point_on_mesh",
             "biot_savart"]
